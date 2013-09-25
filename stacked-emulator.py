6ccd2288200d71f828cc32f3f6c204030c7781a3
Michael Abed <michaelabed@gmail.com>
2013-09-25 00:50:42 -0400

    Split Opcodes into an enum-like class

--- stacked-emulator.py
+++ stacked-emulator.py
@@ -1,3 +1,39 @@
+
+class Opcodes(object):
+    """
+    The varios instructions the CPU understands
+
+    Pulled into own class so other things (assembler, etc) can reference
+    """
+    # INSTRUCTIONS!!! These need to be reviewed and implemented
+    HALT     = 0x0 # EXIT/HALT
+    PUSH     = 0x1 # push from memory location
+    PUSHI    = 0x2 # push an immediate value
+    STORE    = 0x3 # store TOS to memory location
+    DUP      = 0x4 # Duplicate TOS
+    OVER     = 0x5 # Duplicate second element onto the top of the stack
+    SWAP     = 0x6 # Swap top two elements of the stack
+    POP      = 0x7 # POP TOS and drop it on the floor
+    POPDR    = 0x8 # pop from data stack to return stack [FIXME: IMPLEMENT]
+    POPRD    = 0x9 # pop from return stack to data stack [FIXME: IMPLEMENT]
+
+    BEZ      = 0xa # Branch If Equal to Immediate
+    BNEZ     = 0xb # Branch If Not Equal to Immediate
+    CALL     = 0xc # Put current PC ontop of return stack and jump [FIXME: IMPLEMENT]
+    RET      = 0xd # return [FIXME: IMPLEMENT]
+
+    ALU      = 0xf # ARITHMETIC STUFF
+    ALU_SLL  = 0x0 # n2 << n1 [FIXME: IMPLEMENT]
+    ALU_SRA  = 0x1 # n2 >> n1 (maintain sign) [FIXME: IMPLEMENT]
+    ALU_SRL  = 0x2 # n2 >> n1 (push in zeros) [FIXME: IMPLEMENT]
+    ALU_ADD  = 0x3 # n2 + n1 = n3 
+    ALU_SUB  = 0x4 # n2 - n1 = n3 
+    ALU_OR   = 0x5 # n2 | n1 = n3 [FIXME: IMPLEMENT]
+    ALU_AND  = 0x6 # n2 & n1 = n3 [FIXME: IMPLEMENT]
+    ALU_INV  = 0x7 # ~n1 = n2 [FIXME: IMPLEMENT]
+    ALU_NEG  = 0x8 # -n1 = n2 [FIXME: IMPLEMENT]
+
+
 class StackedEmulator(object):
     """
     Stacked is a basic stack machine emulator. The memory architecture and structure
@@ -22,33 +58,6 @@ class StackedEmulator(object):
         self.rstack = [] # return stack
         self.ir = 0      # current instruction register
 
-        # INSTRUCTIONS!!! These need to be reviewed and implemented
-        self.__halt     = 0x0 # EXIT/HALT
-        self.__push     = 0x1 # push from memory location
-        self.__pushi    = 0x2 # push an immediate value
-        self.__store    = 0x3 # store TOS to memory location
-        self.__dup      = 0x4 # Duplicate TOS
-        self.__over     = 0x5 # Duplicate second element onto the top of the stack
-        self.__swap     = 0x6 # Swap top two elements of the stack
-        self.__pop      = 0x7 # POP TOS and drop it on the floor
-        self.__popdr    = 0x8 # pop from data stack to return stack [FIXME: IMPLEMENT]
-        self.__poprd    = 0x9 # pop from return stack to data stack [FIXME: IMPLEMENT]
-
-        self.__bez      = 0xa # Branch If Equal to Immediate
-        self.__bnez     = 0xb # Branch If Not Equal to Immediate
-        self.__call     = 0xc # Put current PC ontop of return stack and jump [FIXME: IMPLEMENT]
-        self.__ret      = 0xd # return [FIXME: IMPLEMENT]
-
-        self.__alu     = 0xf # ARITHMETIC STUFF
-        self.__alu_sll = 0x0 # n2 << n1 [FIXME: IMPLEMENT]
-        self.__alu_sra = 0x1 # n2 >> n1 (maintain sign) [FIXME: IMPLEMENT]
-        self.__alu_srl = 0x2 # n2 >> n1 (push in zeros) [FIXME: IMPLEMENT]
-        self.__alu_add = 0x3 # n2 + n1 = n3 
-        self.__alu_sub = 0x4 # n2 - n1 = n3 
-        self.__alu_or  = 0x5 # n2 | n1 = n3 [FIXME: IMPLEMENT]
-        self.__alu_and = 0x6 # n2 & n1 = n3 [FIXME: IMPLEMENT]
-        self.__alu_inv = 0x7 # ~n1 = n2 [FIXME: IMPLEMENT]
-        self.__alu_neg = 0x8 # -n1 = n2 [FIXME: IMPLEMENT]
 
     def fetch(self):
         """
@@ -64,32 +73,32 @@ class StackedEmulator(object):
         imm = (self.ir & 0x0fff)
         state = 1
         self.pc += 1
-        if opcode == self.__halt:
+        if opcode == Opcodes.HALT:
             print "HALT"
             self.state = 0
             self.pc -= 1
-        elif opcode == self.__push:
+        elif opcode == Opcodes.PUSH:
             print "PUSHI {0}".format(self.dmem[imm])
             self.dstack.append(self.dmem[imm])
-        elif opcode == self.__pushi:
+        elif opcode == Opcodes.PUSHI:
             print "PUSH {0}".format(imm)
             self.dstack.append(imm)
-        elif opcode == self.__store:
+        elif opcode == Opcodes.STORE:
             self.dmem[imm] = self.dstack.pop()
             print "STORE {0}".format(self.dmem[imm])
-        elif opcode == self.__dup:
+        elif opcode == Opcodes.DUP:
             self.dstack.append(self.dstack[-1]) # duplicate the TOS
             print "DUP"
-        elif opcode == self.__over:
+        elif opcode == Opcodes.OVER:
             self.dstack.append(self.dstack[-2])
             print "OVER"
-        elif opcode == self.__swap:
+        elif opcode == Opcodes.SWAP:
             n1 = self.dstack.pop()
             n2 = self.dstack.pop()
             self.dstack.append(n2)
             self.dstack.append(n1)
             print("SWAP {0} {1}".format(n1, n2))
-        elif opcode == self.__bez:
+        elif opcode == Opcodes.BEZ:
             print "BZE",
             res = self.dstack.pop()
             if (res == 0):
@@ -97,7 +106,7 @@ class StackedEmulator(object):
                 self.pc = imm
             else:
                 print "NO JUMP"
-        elif opcode == self.__bnez:
+        elif opcode == Opcodes.BNEZ:
             print "BNZE",
             res = self.dstack.pop()
             if (res != 0):
@@ -105,14 +114,14 @@ class StackedEmulator(object):
                 self.pc = imm
             else:
                 print "NO JUMP"
-        elif opcode == self.__alu:
-            if imm == self.__alu_add:
+        elif opcode == Opcodes.ALU:
+            if imm == Opcodes.ALU_ADD:
                 one = self.dstack.pop()
                 two = self.dstack.pop()
                 res = (two + one) & 0xFF
                 print "{0} {1} ADD = {2}".format(one, two, res)
                 self.dstack.append(res)
-            elif imm == self.__alu_sub:
+            elif imm == Opcodes.ALU_SUB:
                 one = self.dstack.pop()
                 two = self.dstack.pop()
                 res = (two - one) & 0xFF
